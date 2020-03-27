c6be6a64bce17bbbee1f7965e831ed7eac6f230e
Gauthier Mayer <legrangm@gmail.com>
2020-03-27 16:49:51 +0100

    Documentation API

---
+++ api_doc/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "API pour le serveur.",
+  "version": "0.1.0",
+  "description": "API servant d'inferface entre la base de données et le serveur de TPE",
+  "title": "API serveur",
+  "url": "",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2020-03-27T15:16:42.230Z",
+    "url": "http://apidocjs.com",
+    "version": "0.20.1"
+  }
+});
